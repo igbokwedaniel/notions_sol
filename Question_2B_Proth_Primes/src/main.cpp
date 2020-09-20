0d27604063bac76aa60178575b549980166121ad
daniel igbokwe <igbokwedaniel@outlook.com>
2020-09-20 23:04:11 +0100

    Tze's flawed paper: delete second checks

--- Question_2B_Proth_Primes/src/main.cpp
+++ Question_2B_Proth_Primes/src/main.cpp
@@ -9,9 +9,13 @@ int main(int argc, char *argv[])
 
 
 	proth_rand_generator prand_generator{1};
-	proth_number N{17};
+	proth_number N{1};
 	proth_prime_checker<proth_rand_generator> prime_checker{prand_generator,N};
 	
+	long long input;
+	std::cin >> input;
+
+	prime_checker.proth_num.N = input;
 
 	std::cout << prime_checker.run();
 }
--- Question_2B_Proth_Primes/src/proth_primality_checker.cpp
+++ Question_2B_Proth_Primes/src/proth_primality_checker.cpp
@@ -18,17 +18,35 @@ proth_type proth_prime_checker<proth_rand_generator>::run()
     random_gen.prepare();
 
     long long sample_size{powa(std::log2(proth_num.N),2)*proth_num.t};  
-    long long start{0}, a{0}, b{0}, exponent{(proth_num.N-1)/2};
+    long long start{0}, 
+                a{0}, 
+                b{0}, 
+                b_2{0}, 
+                exponent{(proth_num.N-1)/2};
 
 
     while (start <= sample_size){
-        a = random_gen.get_next_rand();
-        b = powa_modulo(a,exponent,proth_num.N);
+        a       = random_gen.get_next_rand();
+        b       = powa_modulo(a,exponent,proth_num.N) - proth_num.N;
+    
+        std::cout<<  "a,b => " << a << " " << b  << std::endl;
 
-        std::cout<<  "a,b => " << a << " " << b << std::endl;
-        if(b - proth_num.N == -1)
+
+        if(b == -1)
             return proth_type::PRIME;
+
+        b_2 = powa_modulo(b,2,proth_num.N);
+
+        std::cout<<  "b^2 mod N => " << b_2 << std::endl;
+
+        if(b_2 != 1)
+            return proth_type::COMPOSITE;
         
+        if(b == 1 and b_2 == 1)
+            return proth_type::COMPOSITE;
+
+        
+
         ++start;
     }
 
